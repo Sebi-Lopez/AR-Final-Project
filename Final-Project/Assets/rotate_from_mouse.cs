1f6ea1ba4983bdfce79b97ffcbac5bc2b043692f
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-05-20 01:47:00 +0200

    lauching ball from the proper direction

--- Final-Project/Assets/rotate_from_mouse.cs
+++ Final-Project/Assets/rotate_from_mouse.cs
@@ -8,27 +8,40 @@ public class rotate_from_mouse : MonoBehaviour
     private GameObject arrow;
     private GameObject ball;
 
-    #region vars
-    public float horizontalSpeed = 200;
-    public float verticalSpeed = 200;
-    bool changeH_dir = false;
-    bool changeV_dir = false;
-    public float verticalLimits=20;
-    #endregion
-
+    [SerializeField]
+    private GameObject direction_bar;
     enum SHOT
     {
         HORIZONTAL,
         VERTICAL,
         INTENSITY,
-        LAUNCH
+        LAUNCH,
+        LAUNCEHD
     }
     SHOT launch_states = SHOT.HORIZONTAL;
+
+    #region vars
+    public float horizontalSpeed = 200;
+    public float verticalSpeed = 200;
+    private bool changeH_dir = false;
+    private bool changeV_dir = false;
+    public float verticalLimits=20;
+    private int times_pressed = 0;
+    private bool pressed = false;
+    //private Transform arrow_trans;
+    private Vector3 initial_arrow_scale;
+    private float max_intensity;
+    private int max_time_pressed = 500;
+    private Transform arrow_trans;
+    #endregion
+
+
     void Start()
     {
         arrow = GameObject.FindGameObjectWithTag("Arrow");
         ball = GameObject.FindGameObjectWithTag("Ball");
-
+        arrow_trans = arrow.GetComponent<Transform>();
+        initial_arrow_scale = arrow_trans.lossyScale;
     }
 
 
@@ -45,11 +58,15 @@ public class rotate_from_mouse : MonoBehaviour
                 break;
 
             case SHOT.INTENSITY:
+                Intensity();
                 break;
 
             case SHOT.LAUNCH:
                 LaunchBall();
                 break;
+            case SHOT.LAUNCEHD:
+                break;
+
         }
 
 
@@ -60,22 +77,70 @@ public class rotate_from_mouse : MonoBehaviour
             
             else if (launch_states == SHOT.VERTICAL)
                 //launch_states = SHOT.INTENSITY;
-                launch_states = SHOT.LAUNCH;
+                launch_states = SHOT.INTENSITY;
             
         }
 
+    }
+
+    void Intensity()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          
+            pressed = true;
+
+        }
+
+        if(Input.GetKey(KeyCode.Space)&& pressed)
+        {
+            if (max_time_pressed > times_pressed)
+            {
+                times_pressed += 1;
+                VisualFeedBack();
+            }
+           // Debug.Log(times_pressed);
+           
+        }
+        else if(Input.GetKeyUp(KeyCode.Space) && times_pressed >0)
+        {
+            launch_states = SHOT.LAUNCH;
+            arrow_trans.lossyScale.Scale(initial_arrow_scale);
+        }
+        
+    }
+
+    void VisualFeedBack()
+    {
+        //conversio dels times a la escala
+
+        double updated_scale = times_pressed * ( initial_arrow_scale.y) / 500;
+        Debug.Log(updated_scale);
+        arrow_trans.lossyScale.Set(arrow_trans.lossyScale.x, (float)updated_scale, arrow_trans.lossyScale.z);
+
+
+       //Vector3 scala = max_time_pressed* arr
     }
 
     void LaunchBall()
     {
         launchball script = ball.GetComponent<launchball>();
         Vector3 vec;
-        vec.x = 0;
+        vec.x = arrow.transform.rotation.y;
         vec.y = arrow.transform.rotation.x;
         vec.z = arrow.transform.rotation.z;
-        Vector3 vv = arrow.transform.eulerAngles;
-        script.ThrowBall(vec);
-        
+        //vec.Normalize();
+        launch_states = SHOT.LAUNCEHD;
+
+        Vector3 rotated_vec;
+        rotated_vec.x = vec.x;
+        float rad_angle = 50 * Mathf.PI / 180;
+        rotated_vec.y = vec.y * Mathf.Cos(rad_angle)-vec.z*Mathf.Sin(rad_angle);
+        rotated_vec.z = vec.y * Mathf.Sin(rad_angle) + vec.z * Mathf.Cos(rad_angle);
+        script.ThrowBall(rotated_vec*10,1);
+
+
+        // 50* Mathf.PI / 180
     }
     void ZAxisRot()
      {
