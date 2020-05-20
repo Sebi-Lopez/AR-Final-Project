192e45cbcbe46d62a2a097b4ec685b161a91646f
Sebi-Lopez <sebi8818@gmail.com>
2020-05-20 03:41:18 +0200

    Reset Ball First try

--- Final-Project/Assets/launchball.cs
+++ Final-Project/Assets/launchball.cs
@@ -38,7 +38,8 @@ public class launchball : MonoBehaviour
         rigid_b.AddForce(dir.x, dir.y, dir.z, ForceMode.Impulse);
         thrown = true;
     }
-    void ResetBall()
+
+    public void ResetBall()
     {
         rigid_b.velocity = Vector3.zero;
         rigid_b.useGravity = false;
@@ -46,4 +47,18 @@ public class launchball : MonoBehaviour
         transform.rotation = Quaternion.identity;
         rotator.SetActive(true);
     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Lose Collider"))
+        {
+           GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>().Miss();
+        }
+        else if(collision.gameObject.CompareTag("Cup Collider"))
+        {
+            GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>().Hit();
+        }
+    }
 }
+
